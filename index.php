6de14384a0fcba88329753ff7668e6863d656a8a
M.C. Brouwer <matthijs@artandflywork.org>
2019-05-23 16:10:05 +0200

    wip

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 
 Kirby::plugin('reprovinci/kirby-upload-duplicates', [
   'hooks' => [
-    'file.create:before' => function ($file, $upload) {
-      // your code goes here
+    'file.create:after' => function ($file, $upload) {
+    	$i = 1;
+    	$tpl = $file->template();
+    	$newname = $tpl . '-' . $file->name() . '-' . $i;
+	    $newpath = str_replace('/' . $file->name(), '/' . $newname, $file->root());
+    	while (F::exists($newpath)) {
+    		$i++;
+    		$newname = $tpl . '-' . $file->name() . '-' . $i;
+	    	$newpath = str_replace('/' . $file->name(), '/' . $newname, $file->root());
+    	}
+    		
+    	$file->changeName($newname);
+
     }
   ]
 ]);
-  
